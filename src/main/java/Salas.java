3591cc32ff582758f11de36f95681f5485e045eb
Ian Cooper <ian.cooper@usm.cl>
2024-07-01 03:50:08 -0400

    Add salas class (read, update)

---
+++ src/main/java/Salas.java
@@ -0,0 +1,57 @@
+public class Salas {
+
+    private int codeId;
+    private String name;
+    private String location;
+    private boolean isUsed;
+
+    public Salas(int code_id, String name, String location, boolean isUsed) {
+        this.codeId = code_id;
+        this.name = name;
+        this.location = location;
+        this.isUsed = isUsed;
+    }
+
+    public Salas(int code_id, String name, String location) {
+        this.codeId = code_id;
+        this.name = name;
+        this.location = location;
+    }
+
+    public Salas() {
+        this.isUsed = false;
+    }
+
+    public int getCodeId() {
+        return codeId;
+    }
+
+    public void setCodeId(int codeId) {
+        this.codeId = codeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean getIsUsed() {
+        return isUsed;
+    }
+
+    public void setIsUsed(boolean isUsed) {
+        this.isUsed = isUsed;
+    }
+
+}
